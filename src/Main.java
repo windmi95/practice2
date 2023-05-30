d78d9588dccce74e2168d4135a87405dead03882
LMH <windmi95@nate.com>
2023-05-30 19:00:40 +0900

    intelij_variable

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int a=3;
+        int b=9;
+
+        int b1 = a+b;
+
+        String c="정답은 ";
+        String d="이것입니다.";
+       // +9223372036854775807L 정답은 이것입니다.
+
+        b = 15;
+
+        System.out.println(a+b1+b+c+d);
+
+
+
+    }
+}
